389fc74cd65a20bfe4ca34dfa4b19555134b5e7d
LaVette Clifton <lavette.clifton@gmail.com>
2018-02-08 23:47:59 -0500

    list added

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  template: `                           
+  <input [value]="title"              
+         (keyup.enter)="changeTitle($event.target.value)"
+         #inputElement>
+         
+  <button (click)="changeTitle(inputElement.value)">
+    Save
+  </button>
+  <p>You input: {{ title }}</p>
+`,
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  title: string = 'todo-list item';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeTitle(newTitle: string): void {
+    this.title = newTitle;
+  }
+
+}
